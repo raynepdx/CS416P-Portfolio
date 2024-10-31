1ffce67fc6d8659642d6faa8d603fef7221ffa7d
raynepdx <conallen@pdx.edu>
2024-10-31 12:31:54 -0700

    Create audio_visualizer.py

---
+++ code/fft_stuff/audio_visualizer.py
@@ -0,0 +1,6 @@
+#this program is going to take in an audio file and create a visual representation
+#of the audio file using the fast fourier transform
+#to help with this I used the following documentation: https://matplotlib.org/3.5.3/api/_as_gen/matplotlib.pyplot.html
+
+import numpy as np
+import matplotlib.pyplot as plt
